c2af35fe8cb45c9f02812a13537d23dc86154e41
iiaii <jhun9409@naver.com>
2021-10-10 23:30:50 +0900

    test, feat: 단일 자동차 생성, 이동 테스트 및 구현

---
+++ src/main/java/racinggame/domain/Car.java
@@ -0,0 +1,45 @@
+package racinggame.domain;
+
+import java.util.Objects;
+
+public class Car {
+
+    private static final Position MOVING_UNIT = new Position(1);
+
+    private final CarName carName;
+    private Position position;
+
+    public Car(final String name) {
+        this.carName = new CarName(name);
+        this.position = new Position();
+    }
+
+    public Position move(final MovingStrategy movingStrategy) {
+        if (movingStrategy.movable()) {
+            position = position.add(MOVING_UNIT);
+        }
+        return position;
+    }
+
+    public Position currentPosition() {
+        return position;
+    }
+
+    public String name() {
+        return carName.name();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Car car = (Car) o;
+        return Objects.equals(carName, car.carName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carName);
+    }
+
+}
---
+++ src/test/java/racinggame/domain/CarTest.java
@@ -0,0 +1,54 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @Test
+    @DisplayName("자동차 생성 - 자동차 비교, 현재 위치 0")
+    public void 자동차생성() {
+        // given
+        String name = "abc";
+        Position expectedPosition = new Position(0);
+
+        // when
+        Car car = new Car(name);
+        Position position = car.currentPosition();
+
+        // then
+        assertThat(car).isEqualTo(new Car(name));
+        assertThat(position).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    @DisplayName("자동차 이동 - 이동 가능한 경우")
+    public void 자동차이동_이동가능한경우() {
+        // given
+        Car car = new Car("abc");
+        Position expectedPosition = new Position(1);
+
+        // when
+        Position position = car.move(() -> true);
+
+        // then
+        assertThat(position).isEqualTo(expectedPosition);
+    }
+
+    @Test
+    @DisplayName("자동차 이동 - 이동 불가능한 경우")
+    public void 자동차이동_이동불가능한경우() {
+        // given
+        Car car = new Car("abc");
+        Position expectedPosition = new Position(0);
+
+        // when
+        Position position = car.move(() -> false);
+
+        // then
+        assertThat(position).isEqualTo(expectedPosition);
+    }
+
+}
