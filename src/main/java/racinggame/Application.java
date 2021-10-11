40608476adebad0428c08d7748a6a9c415c75c90
iiaii <jhun9409@naver.com>
2021-10-11 23:46:14 +0900

    feat, refactor: 자동차 경주 메인 구현 및 테스트 실행 시간 변경

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,34 @@
 package racinggame;
 
+import racinggame.domain.Cars;
+import racinggame.domain.Position;
+import racinggame.domain.RandomMovingStrategy;
+import racinggame.domain.Round;
+import racinggame.dto.RoundResultDto;
+import racinggame.dto.WinnerNamesDto;
+import racinggame.exception.RepeatableException;
+import racinggame.view.*;
+
+import java.util.List;
+import java.util.function.Supplier;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO 자동차 경주 게임 구현
+        Cars cars = repeatIfError(() -> new Cars(CarNameConsoleInput.askCarNames())) ;
+        Round round = repeatIfError(() -> new Round(RoundConsoleInput.askCarNames()));
+        List<List<Position>> roundPositions = round.repeat(() -> cars.moveCars(new RandomMovingStrategy()));
+        RoundResultConsoleOutput.printRoundResults(new RoundResultDto(cars.carNames(), roundPositions));
+        RaceResultConsoleOutput.printRaceResult(new WinnerNamesDto(cars.winnerCarNames()));
     }
+
+    private static <T> T repeatIfError(final Supplier<T> supplier) {
+        try {
+            return supplier.get();
+        } catch(RepeatableException e) {
+            ErrorMessageConsoleOutput.print(e.getMessage());
+            return repeatIfError(supplier);
+        }
+    }
+
 }
